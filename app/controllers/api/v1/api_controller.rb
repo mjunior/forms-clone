4f324712f9bfacf80aa51e1dc6e534edfd039699
mjunior <mauriciojs.junior@gmail.com>
2018-01-20 20:56:05 -0200

    Config error handler

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,5 +1,10 @@
-module Api::V1::
-  class ApiController < ::ApplicationController
-    #
+module Api::V1::ApiController < ::ApplicationController
+  
+  rescue_from(ActiveRecord::RecordNotFound) do ||
+    render(json: {message: 'Not Found'}, status: :not_found)
+  end
+
+  rescue_from(ActionController::ParameterMissing) do |parameter_missing_exception|
+    render(json: {message: "Required parameter missing: #{parameter_missing_exception.param}"}, status: :bad_request)
   end
 end
--- app/controllers/api/v1/forms_controller.rb
+++ app/controllers/api/v1/forms_controller.rb
@@ -1,4 +1,4 @@
-class Api::V1::FormsController < ApplicationController
+class Api::V1::FormsController < Api::V1::ApiController
   def index
   end
 
